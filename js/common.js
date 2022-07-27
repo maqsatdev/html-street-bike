1389a25b255b5666c6c1d785a7749ccad3c2203f
Максат Куанышов <maqsat@MacBook-Air-Maksat.local>
2022-07-27 18:00:38 +0600

    update

--- js/common.js
+++ js/common.js
@@ -10,4 +10,32 @@ document.addEventListener("DOMContentLoaded", function () {
   hamburgerBtn.addEventListener("click", function () {
     this.classList.toggle("is-active");
   });
+
+  // Bike Carousel
+  const bikeCarousel = new Swiper(".bike_carousel", {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
+
+  // Images Carousel
+  const imagesCarousel = new Swiper(".img_slider", {
+    slidesPerView: 1,
+    effect: "fade",
+    speed: 2500,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
 });
